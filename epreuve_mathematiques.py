db01c1e44b0fba438f84dd3b288f069dfb832c23
Meya18 <meya.fall@efrei.net>
2024-12-06 12:55:56 +0100

    ajout nombre premier

--- epreuve_mathematiques.py
+++ epreuve_mathematiques.py
@@ -67,6 +67,30 @@ def epreuve_roulette_mathematique():
     else:
         print(f"Incorrect! La bonne réponse est {bonne_reponse}")
 
+def est_premier(n):
+    nb_premier=False
+    if n<=1:
+        return nb_premier
+    for i in range(2,n):
+        if n%i==0:
+            nb_premier=False
+    return nb_premier
+def premier_plus_proche(n):
+    while not est_premier(n):
+        n=n+1
+    return n
+
+def epreuve_math_premier():
+    n = random.randint(10, 20)
+    print("Saisir le nombre de premier le plus proche de {}".format(n))
+    reponse = input("Entrer votre réponse: ")
+    if premier_plus_proche(n) == reponse:
+        return True
+    else:
+        return False
+
+
+epreuve_math_premier()
 
 #Fonction epreuve_math()
 def epreuve_math():
